a31fb08c4130a52377d0de558b2ec06d727f15a8
andrejkompanec9-bit <andrejkompanec9@gmail.com>
2026-04-14 16:57:24 +0300

    увольнения

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        for (int i = 2; i <= 100; i = i + 2){
+                System.out.println("Уволен айтишник с id" + i);
+        }
     }
 }
+
+/* ТОЖЕ САМОЕ ЧЕРЕЗ WHILE
+public class Main {
+    public static void main(String[] args) {
+
+       int i = 2
+       while (i <= 100){
+       System.out.println("Уволен айтишник с id" + i);
+       i += 2;       аналогично i = i +2
+ */
